b03d9f8ff5d935f4f2f955bd8104e51bfc6f104e
Ali Bahrami <ali.bahraminezhad@outlook.com>
2019-11-24 17:46:28 +0100

    Add simple flask Dockerfile

---
+++ app/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World - From a container! <br/> <img src="www\img\hello.png" />
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
