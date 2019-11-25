477a7dbf1829b7c49a3429ea28e80b54d624a29e
Ali Bahraminezhad <ali.bahraminezhad@outlook.com>
2019-11-25 14:14:40 +0100

    Add compose and nginx to serve static files

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World - From a container! <br/> <img src="www\img\hello.png" />
+    return 'Hello, World - From a container! <br/> <img src="static/img/hello.png" />'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
